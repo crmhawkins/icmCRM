13bf7c384b4ff40f9f5db54e2d58788331119d4b
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2025-01-21 10:05:28 +0100

    Update index.blade.php

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -52,6 +52,7 @@
 
 @section('scripts')
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/google-calendar@6.1.15/index.global.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.15/locales-all.global.min.js"></script>
     @include('partials.toast')
     <script>
