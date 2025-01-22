1bd9574f93393232a6c1f0a44ba91ee43aba35c2
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2025-01-22 10:48:09 +0100

    cambios

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -52,12 +52,7 @@
                     $comercial = (Auth::user()->access_level_id == 6);
                     @endphp
 
-                <li class="sidebar-item {{ request()->routeIs('calendar.index') ? 'active' : '' }}">
-                    <a href="{{route('calendar.index')}}" class='sidebar-link'>
-                        <i class="fa-solid fa-calendar fs-5"></i>
-                        <span>Calendario</span>
-                    </a>
-                </li>
+
                 <li class="sidebar-item has-sub {{ $clientesActive ? 'active' : '' }}">
                     <a href="#" class='sidebar-link'>
                         <i class="fa-solid fa-people-group fs-5"></i>
@@ -779,7 +774,18 @@
                         <span>Archivos</span>
                     </a>
                 </li>
-
+                <li class="sidebar-item {{ request()->routeIs('calendar.index') ? 'active' : '' }}">
+                    <a href="{{route('calendar.index')}}" class='sidebar-link'>
+                        <i class="fa-solid fa-calendar fs-5"></i>
+                        <span>Calendario</span>
+                    </a>
+                </li>
+                <li class="sidebar-item {{ request()->routeIs('backup.index') ? 'active' : '' }}">
+                    <a href="{{route('backup.index')}}" class='sidebar-link'>
+                        <i class="fa-solid fa-box-archive fs-5"></i>
+                        <span>Backup</span>
+                    </a>
+                </li>
 
             </ul>
             <div class="sidebar-footer mt-3">
@@ -798,5 +804,36 @@
             </div>
         </div>
         {{-- <button class="sidebar-toggler btn x"><i data-feather="x"></i></button> --}}
+        <button type="button" class="btn btn-outline-secondary mt-1" data-bs-toggle="modal" data-bs-target="#textoModal">
+            Mostrar Texto
+          </button>
+        </div>
     </div>
-</div>
+    <div class="modal fade" id="textoModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalLabel">Texto del Modal</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    En {{env('EMPRESA_DESAROLLADORA')}} <b>desarrollamos y ofrecemos</b> una solución integral para la gestión y emisión de facturas electrónicas diseñada para empresas que buscan eficiencia y cumplimiento normativo. Nuestra plataforma ofrece una interfaz amigable y potentes herramientas para automatizar el proceso de facturación, asegurando la conformidad con las regulaciones locales e internacionales.
+                    <br><br>
+                    <b>Funcionalidades Clave:</b>
+                    <br>•<b>Gestión de Facturas Ilimitadas:</b> Cree y gestione un número ilimitado de facturas sin restricciones adicionales, optimizando su flujo de trabajo de facturación.
+                    <br>•<b>Alta de Clientes Ilimitada:</b> Registre y administre una cantidad ilimitada de clientes, permitiendo una expansión y adaptabilidad constante a las necesidades de su negocio.
+                    <br>•<b>Alta de Productos y Servicios Ilimitada:</b> Añada y actualice continuamente productos y servicios sin enfrentar limitaciones en la cantidad, lo que facilita la adaptación a las dinámicas de mercado y la expansión del inventario.
+                    <br>•<b>Automatización y Personalización:</b> Automatice procesos repetitivos y personalice facturas con su logotipo y datos empresariales, mejorando la presentación y profesionalismo.
+                    <br>•<b>Seguridad y Conformidad:</b> Garantizamos la seguridad de sus datos mediante cifrado avanzado y nos aseguramos de que nuestra plataforma esté siempre actualizada con las últimas normativas de facturación electrónica.
+                    <br><br>
+                    <b>Beneficios:</b>
+                    <br>•<b>Eficiencia Operativa:</b> Reduzca el tiempo de gestión de facturas y clientes con nuestras herramientas automatizadas.
+                    <br>•<b>Escalabilidad:</b> Nuestro sistema se adapta a empresas de cualquier tamaño y puede crecer según sus necesidades sin incurrir en costos adicionales por volumen de datos.
+                    <br>•<b>Acceso Remoto:</b> Acceda a su sistema de facturación desde cualquier lugar y en cualquier momento, facilitando el manejo remoto y en tiempo real de su negocio.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
